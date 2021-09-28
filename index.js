14dcaaa01e75d830ece49c783226adf2f17df651
Maxime Di Natale <maxime@oddiris.com>
2021-09-28 21:39:31 +0200

    second commit!

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 addEventListener("fetch", (event) => {
-    const response = new Response("Hello World!", {
-        headers: { "content-type": "text/plain" },
-    })
+    const response = new Response(
+        JSON.stringify({
+            bonsoir: "papa reviens",
+            grossePute: true,
+        }),
+        {
+            headers: { "content-type": "application/json" },
+        }
+    )
     event.respondWith(response)
 })
